5b99554e38818bd4a3a03d86cc93e3a5e935e0fd
erickmoses50 <erickmoses2020@gmail.com>
2025-08-15 14:45:03 +0300

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,4 +1,5 @@
-num1 = int(input("Enter your first no here:"))
-num2 = int(input("Enter your second no here:"))
+print("Division of numbers")
+num1 = int(input("Enter your first number here:"))
+num2 = int(input("Enter your second number here:"))
 numeric = num1/num2
 print(numeric)
